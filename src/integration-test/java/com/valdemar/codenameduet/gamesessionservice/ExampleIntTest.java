844439dc8ed81df4330d2c45e9acef1f31219e0a
Valdemar Pereira <valdemar.developer@gmail.com>
2020-04-16 15:47:22 +0100

    Adding a Integration Test profile

---
+++ src/integration-test/java/com/valdemar/codenameduet/gamesessionservice/ExampleIntTest.java
@@ -0,0 +1,14 @@
+package com.valdemar.codenameduet.gamesessionservice;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleIntTest {
+
+  @Test
+  void test(){
+    assertTrue(true);
+  }
+
+}
